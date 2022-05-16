4df8549bbc0d1073566ceeda0fbf91c2ebf124b8
faith <xianzixiang@gmail.com>
2022-05-16 15:13:39 +0800

    feat: finish bug

--- klipper/container/start.sh
+++ klipper/container/start.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+/root/miniconda3/envs/klippy_py/bin/pip install  -r /root/klipper/scripts/klippy-requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
+
+/root/miniconda3/envs/moonraker_py/bin/pip install -r /root/moonraker/scripts/moonraker-requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
+
+
 alias cp="cp"
 echo "start service"
 
